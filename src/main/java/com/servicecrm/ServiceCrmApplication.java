109723ee852accfcf9745524cdf59f8e95dc4c97
stanislavyakunin77-eng <stanislavyakunin77@gmail.com>
2026-06-15 09:22:45 +0300

    Add Spring Boot main application class

---
+++ src/main/java/com/servicecrm/ServiceCrmApplication.java
@@ -0,0 +1,13 @@
+package com.servicecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCrmApplication.class, args);
+    }
+
+}
