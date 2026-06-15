05b6126e9914332a8013204b78a69923817c194c
stanislavyakunin77-eng <stanislavyakunin77@gmail.com>
2026-06-15 09:24:31 +0300

    Add UserRepository

---
+++ src/main/java/com/servicecrm/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.servicecrm.repository;
+
+import com.servicecrm.model.entity.User;
+import com.servicecrm.model.entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(UserRole role);
+    List<User> findByRoleAndIsActiveTrue(UserRole role);
+}
